4aced3410af32411ba528390574c66d6cc0c7554
Sanjeev V Gaikwad <gaikwadsanjeevv@gmail.com>
2024-12-26 03:48:19 -0800

    chaining comparison, string comparison

---
+++ PythonCodes/chaining_comparision_Operator.py
@@ -0,0 +1,4 @@
+age = 22
+#if age >= 18 and age < 65:
+if 18 <= age < 65:   # chaining comparision operator - cleaner code
+    print("Eligible")
---
+++ PythonCodes/greater_string.py
@@ -0,0 +1,6 @@
+if 10 == "10":
+    print("a")
+elif "bag"> "apple" and "cat" > "bull":
+    print("b")
+else:
+    print("c")
