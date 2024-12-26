a0ea6a37ae7aa143a4db39b4640b615dd81a5866
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-12-26 03:50:21 -0800

    Update greater_string.py

--- PythonCodes/greater_string.py
+++ PythonCodes/greater_string.py
@@ -1,6 +1,6 @@
 if 10 == "10":
     print("a")
-elif "bag"> "apple" and "cat" > "bull":
+elif "bag"> "apple" and "cat" > "bull":  # alphabetically a is smaller than b than c and so on comparision is done
     print("b")
 else:
     print("c")
