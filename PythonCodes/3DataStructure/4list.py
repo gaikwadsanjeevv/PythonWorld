8da3ee3b6aeb4b874ac9c455285b9fd3c2a763d1
Sanjeev V Gaikwad <gaikwadsanjeevv@gmail.com>
2025-04-30 15:24:26 -0700

    Create 4list.py

---
+++ PythonCodes/3DataStructure/4list.py
@@ -0,0 +1,27 @@
+# List are ordered, mutable collections of items
+# They can contain items of different data types.
+
+
+lst = []
+print(type(lst))
+names = ["Krish", "Jack", "Jacob", 1, 2, 3, 4, 5]
+print(names)
+
+##Accessing List Elements: 
+fruits = ["apple", "banana","cherry", "kiwi", "gauva"]
+print(fruits[0])
+print(fruits[-1])
+print(fruits[0:3])
+print(fruits[1:3])
+
+
+# modifying list elements
+fruits[1] = "watermelon"
+print(fruits)
+
+fruits[1:] = "sanjeev"
+print(fruits)
+
+fruits2 = ["apple", "banana","cherry", "kiwi", "gauva"]
+#List method
+
